5b674b9aca32709125b724031023e2eda26bb428
Hossein <ho3einsg@gmail.com>
2023-06-21 13:57:45 +0330

    fix package version bugs

--- lib/src/ui/epub_view.dart
+++ lib/src/ui/epub_view.dart
@@ -359,7 +359,7 @@ class _EpubViewState extends State<EpubView> {
           //               document.Content!.Images![url]!.Content!,
           //             ),
           //           ),
-          //         );
+          //
           //       },
           //     ),
           //   ),
